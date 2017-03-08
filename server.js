9bca51812cbdaa4898c4fdae7eddc6543ec05a12
Richard Wurth <Richard Wurth>
2017-03-07 20:28:50 -0700

    Initial and final commit for Siri

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+app.listen(8887, function() {
+  console.log('Listening on port 8585');
+});
+const messages = ["Unë nuk e kuptoj", "わかりません", "не разумею","Eu não entendo","No entiendo"];
+function getRandomMessage () {
+  const randomNum = Math.floor(Math.random()* messages.length);
+  return messages[randomNum];
+};
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
+    message: getRandomMessage()
+  }));
+});
